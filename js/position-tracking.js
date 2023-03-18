f0f037101eeda2110ce0a54922474ab2bc5825f4
Gennady Uraltsev <genandy.uraltsev@gmail.com>
2023-03-17 21:00:21 -0400

    Comment on face disposition. Make faces smaller

--- js/position-tracking.js
+++ js/position-tracking.js
@@ -1,15 +1,13 @@
 AFRAME.registerComponent("position-tracking", {
   // A new instance of the component gets created for each <a-entity> the component is attached to
-
-  // The init function gets executed each time an instance of the component is created
+  // The given instance of the component can be accessed using the `this` variable
+  // - this.name refers to the string representation of the component name
   init: function () {
-    // The given instance of the component can be accessed using the `this` variable
-    // for example: this.name refers to the string representation of the component name
+    // The init function gets executed each time an instance of the component is created
     console.log(
-      "Hello, " + this.name + " is being initialized on the element."
+      "Hello, " + this.name + " is being initialized on the element"
     );
-    // this.el refers to the <a-entity> this component is attached to
-    console.log(this.el);
+    console.log(this.el); // this.el is the <a-entity> this component is attached to
 
     this.debug = AFRAME.utils.throttle(this.debug, 1000, this);
   },
@@ -27,50 +25,50 @@ AFRAME.registerComponent("position-tracking", {
     this.el.object3D.position.y;
     this.el.object3D.position.z;
 
-    if (this.el.object3D.position.x < 10 && this.el.object3D.position.x > -10) {
+    if (this.el.object3D.position.x < 10/2 && this.el.object3D.position.x > -10/2) {
       if (
-        this.el.object3D.position.z < 10 &&
-        this.el.object3D.position.z > -10
+        this.el.object3D.position.z < 10/2 &&
+        this.el.object3D.position.z > -10/2
       ) {
         current_face = "FRONT";
       } else if (
-        this.el.object3D.position.z < -10 &&
-        this.el.object3D.position.z > -30
+        this.el.object3D.position.z < -10/2 &&
+        this.el.object3D.position.z > -30/2
       ) {
         current_face = "TOP";
       } else if (
-        this.el.object3D.position.z < 30 &&
-        this.el.object3D.position.z > 10
+        this.el.object3D.position.z < 30/2 &&
+        this.el.object3D.position.z > 10/2
       ) {
         current_face = "BOTTOM";
       }
     } else if (
-      this.el.object3D.position.x < -10 &&
-      this.el.object3D.position.x > -20
+      this.el.object3D.position.x < -10/2 &&
+      this.el.object3D.position.x > -20/2
     ) {
       if (
-        this.el.object3D.position.z < 10 &&
-        this.el.object3D.position.z > -10
+        this.el.object3D.position.z < 10/2 &&
+        this.el.object3D.position.z > -10/2
       ) {
         current_face = "LEFT";
       }
     } else if (
-      this.el.object3D.position.x < 30 &&
-      this.el.object3D.position.x > 10
+      this.el.object3D.position.x < 30/2 &&
+      this.el.object3D.position.x > 10/2
     ) {
       if (
-        this.el.object3D.position.z < 10 &&
-        this.el.object3D.position.z > -10
+        this.el.object3D.position.z < 10/2 &&
+        this.el.object3D.position.z > -10/2
       ) {
         current_face = "RIGHT";
       }
     } else if (
       this.el.object3D.position.x < 50 &&
-      this.el.object3D.position.x > 30
+      this.el.object3D.position.x > 30/2
     ) {
       if (
-        this.el.object3D.position.z < 10 &&
-        this.el.object3D.position.z > -10
+        this.el.object3D.position.z < 10/2 &&
+        this.el.object3D.position.z > -10/2
       ) {
         current_face = "BACK";
       }
